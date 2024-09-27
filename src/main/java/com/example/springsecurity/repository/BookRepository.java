7d8cb498c57b8f1187e2ff5bc5c2cc990c920965
NguyenThanhhToan <toanthanhnguyen2302@gmail.com>
2024-09-27 19:13:45 +0700

    add method findByUser for BookRepository

--- src/main/java/com/example/springsecurity/repository/BookRepository.java
+++ src/main/java/com/example/springsecurity/repository/BookRepository.java
@@ -1,10 +1,15 @@
 package com.example.springsecurity.repository;
 
+import com.example.springsecurity.model.dto.UserDto;
 import com.example.springsecurity.model.entity.Book;
+import com.example.springsecurity.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     boolean existsByTitle(String title);
+    List<Book> findByUser(User user);
 }
